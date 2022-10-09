864914cc65f764258d4b36b02d4c138e131740f1
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-09 09:43:52 +0300

    header component done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./App.scss";
 import Navbar from "./component/Navbar/Navbar";
+import Header from "./component/Header/Header";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Header />
       <h1>Hello</h1>
     </div>
   );
