24917b2fde390779a03a9d013da8bb5a358771df
İsmail Yazıcı <i.yazicigircin18@gmail.com>
2022-10-09 09:30:13 +0300

    create navbar import

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.scss";
+import Navbar from "./component/Navbar/Navbar";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
